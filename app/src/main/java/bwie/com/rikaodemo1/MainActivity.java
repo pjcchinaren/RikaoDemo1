e013f3ee4a46137faccdada5e0109d821694cbfa
pengjincheng <pengjincheng@eakay.cn>
2017-11-28 11:04:19 +0800

    加了一行注释

--- app/src/main/java/bwie/com/rikaodemo1/MainActivity.java
+++ app/src/main/java/bwie/com/rikaodemo1/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        //修改的代码
 
     }
 
